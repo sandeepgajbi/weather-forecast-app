167dbef5b54f67f5dca2d408e5cd6ef8580f8e82
sandeepgajbi <sandeepgajbi.gmail.com>
2024-04-15 21:19:33 +0800

    Add widgets

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Weather Forecast for the Next Days")
+
+place = st.text_input('Place')
+
+days = st.slider('Forecast Days', min_value=1, max_value=5, help="Select the number of forecasted days")
+
+option = st.selectbox('Select the data to view', ('Temperature', 'Sky'))
+
+st.subheader(f"{option.title()} for next {days} days in {place.title()}")
+
